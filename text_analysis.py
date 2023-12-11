76933aa1bf162b491a5b068e5d0b5c49ec8f2bcc
Charleenlozi <clozymwangs@gmail.com>
2023-12-12 00:46:48 +0300

    feat: getting reuters sample text

--- text_analysis.py
+++ text_analysis.py
@@ -1,5 +1,16 @@
 import nltk
+
+from nltk.corpus import reuters
 # nltk.download('reuters')
 # nltk.download('punkt')
 # nltk.download('stopwords')
 # nltk.download('wordnet')
+
+#Sample Data
+sample_text = reuters.raw(fileids=reuters.fileids(categories='crude')[0])
+
+#TOKENIZATION
+"""There are various types of tokenizers provided by nltk that handle splitting sentences  at certian levels, either
+by sentence, word,regex,tweet etc"""
+#sentence_tokenizer
+"""Using PunktSentenceTokenizer"""
