ca283bec67a4eaebcfe881f8b6d9189144cc9ea7
Charleenlozi <clozymwangs@gmail.com>
2023-12-12 02:22:31 +0300

    feat: implementing lemmatization

--- text_analysis.py
+++ text_analysis.py
@@ -1,25 +1,56 @@
+
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+from nltk.tokenize import word_tokenize
 import nltk
 
 from nltk.corpus import reuters
-from nltk.tokenize.punkt import PunktSentenceTokenizer
-from nltk.tokenize import word_tokenize
+
 # nltk.download('reuters')
 # nltk.download('punkt')
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 
-#Sample Data
+# Sample Data
 sample_text = reuters.raw(fileids=reuters.fileids(categories='crude')[0])
 print(sample_text)
 
-#TOKENIZATION
+# TOKENIZATION
 """There are various types of tokenizers provided by nltk that handle splitting sentences  at certian levels, either
 by sentence, word,regex,tweet etc"""
-#sentence_tokenizer
+# sentence_tokenizer, tokenizes texts to sentences at punctuations
 
 pst = PunktSentenceTokenizer()
 print(pst.tokenize(sample_text))
 
-#word tokenizer
+# word tokenizer, splits text/sentences to individual words
 print(word_tokenize(sample_text))
 
+# STEMMING AND lEMMATIZATION
+# Stemming is the process of morphological affixes from a word and reducing it to its stem, which is the root word, it takes in one word at a time.
+stemmer = PorterStemmer()
+
+plurals = ['caresses', 'flies', 'dies', 'mules', 'denied',
+           'died', 'agreed', 'owned', 'humbled', 'sized',
+           'meeting', 'stating', 'siezing', 'itemization',
+           'sensational', 'traditional', 'reference', 'colonizer',
+           'plotted']
+
+singles = [stemmer.stem(plural) for plural in plurals]
+print(singles)
+# can use snawballstemmer to stem words language specific
+
+"""Lemmatization is the process of converting a word to its lemma/base/dictionary form. It generally means transforming the word to its most basic, canonical, or lemma form. This involves stripping away inflections, derivational affixes, and considering the base form regardless of the word's grammatical part of speech.
+
+For example:
+
+Inflections: Converting plurals to singular forms (e.g., "dogs" to "dog").
+Derivational affixes: Stripping away prefixes or suffixes that change the word's meaning (e.g., "unhappiness" to "happy").
+Part of speech: Treating different grammatical forms of a word as a single lemma (e.g., "running" and "ran" reduced to "run").
+helps in standardizing the representation of words"""
+
+from nltk.stem import WordNetLemmatizer
+wnl = WordNetLemmatizer()
+
+print(wnl.lemmatize("dogs"))
