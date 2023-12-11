bd98982278c4807dfe586019c6434d7eb928fa63
Charleenlozi <clozymwangs@gmail.com>
2023-12-12 01:17:43 +0300

    feat: word, sentence tokenization of sample text

--- text_analysis.py
+++ text_analysis.py
@@ -1,6 +1,8 @@
 import nltk
 
 from nltk.corpus import reuters
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+from nltk.tokenize import word_tokenize
 # nltk.download('reuters')
 # nltk.download('punkt')
 # nltk.download('stopwords')
@@ -8,9 +10,16 @@ from nltk.corpus import reuters
 
 #Sample Data
 sample_text = reuters.raw(fileids=reuters.fileids(categories='crude')[0])
+print(sample_text)
 
 #TOKENIZATION
 """There are various types of tokenizers provided by nltk that handle splitting sentences  at certian levels, either
 by sentence, word,regex,tweet etc"""
 #sentence_tokenizer
-"""Using PunktSentenceTokenizer"""
+
+pst = PunktSentenceTokenizer()
+print(pst.tokenize(sample_text))
+
+#word tokenizer
+print(word_tokenize(sample_text))
+
